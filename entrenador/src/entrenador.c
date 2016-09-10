b58b6758d433575c5179882b063fd38f657fa2d4
Julian Alvarez <alvarezjulianignacio@gmail.com>
2016-09-10 09:49:39 -0300

    Proceso entrenador

--- entrenador/src/entrenador.c
+++ entrenador/src/entrenador.c
@@ -23,7 +23,7 @@ t_metadata_entrenador * crear_metadata() {
 	t_metadata_entrenador * metadata = malloc(sizeof(t_metadata_entrenador));
 	memset(metadata, 0, sizeof(t_metadata_entrenador));
 
-	metadata->viaje = list_create();
+	metadata->viaje = queue_create();
 
 	return metadata;
 }
@@ -52,9 +52,9 @@ int leer_metadata_mapa(char * metada_path) {
 		string_append(&(property_name), ciudad);
 		string_append(&(property_name), "]");
 
-		mapa_i->objetivos = getListProperty(conf_file, property_name);
+		mapa_i->objetivos->elements = getListProperty(conf_file, property_name);
 
-		list_add_in_index(metadata->viaje, i, mapa_i);
+		list_add_in_index(metadata->viaje->elements, i, mapa_i);
 
 		free(property_name);
 	}
@@ -78,17 +78,17 @@ void imprimir_metadata() {
 
 	printf("\n**Hoja de viaje**\n");
 
-	int ciudades = list_size(metadata->viaje);
+	int ciudades = list_size(metadata->viaje->elements);
 	int i;
 
 	for(i = 0; i < ciudades; i++) {
-		t_mapa * mapa = list_get(metadata->viaje, i);
+		t_mapa * mapa = list_get(metadata->viaje->elements, i);
 		printf("%s\n", mapa->nombre_mapa);
 		printf("objetivos: [");
-		int objetivos = list_size(mapa->objetivos);
+		int objetivos = list_size(mapa->objetivos->elements);
 		int j;
 		for (j = 0; j < objetivos; j++) {
-			printf(" %s ", (char *) list_get(mapa->objetivos, j));
+			printf(" %s ", (char *) list_get(mapa->objetivos->elements, j));
 		}
 		printf("]\n");
 	}
@@ -123,3 +123,265 @@ int enviar_datos_a_mapa(int socket, char simbolo, char * nombre) {
 
 	return EXIT_SUCCESS;
 }
+
+
+//------------------Señales
+
+void rutina(int signal){
+	switch(signal){
+
+		case SIGUSR1:
+			metadata->vidas += 1;
+			break;
+
+		case SIGTERM:
+			metadata->vidas -= 1;
+			break;
+
+		default: puts("Codigo de señal invalida");
+	}
+}
+
+//-------------------Funciones
+
+
+void inicializarEntrenador(){
+
+	cantidadDeMuertes = 0;
+	metadata->reintentos = 0;
+	inicializarSinmuertesNiReintentos();
+}
+
+void inicializarSinmuertesNiReintentos(){
+
+	socket_entrenador = -1;
+	metadata->viaje = queue_create();
+	ubicacionProximaPokenest = NULL;
+	pokemonesCapturados = list_create();
+	pokemonMasFuerte = NULL;
+	finDelJuego = false;
+	cargarMetadata();
+}
+void cargarMetadata(){
+
+	leer_metadata_mapa(metadata_path);
+	imprimir_metadata();
+}
+
+void conectarseConSiguienteMapa(){
+
+	mapaActual = queue_peek(metadata->viaje);
+
+		socket_entrenador = conectarse_a_un_mapa(mapaActual->puerto, mapaActual->ip);
+
+		if (socket_entrenador < 0) {
+
+			printf("No es posible conectarse con el mapa: %s \n", mapaActual->nombre_mapa);
+
+		} else {
+
+			printf("Conectado con %s \n", mapaActual->nombre_mapa);
+			enviar_datos_a_mapa(socket_entrenador, metadata->simbolo, metadata->nombre);
+
+			ubicacionActual->x = 0;
+			ubicacionActual->y = 0;
+			pokenestLocalizada = false;
+		}
+}
+
+void solicitarUbicacionDelProximoPokenest(){
+	char id_pokemon;							//talvez hay que usar char*
+
+	id_pokemon = queue_pop(mapaActual->objetivos);
+
+	enviarSolicitudUbicacionPokenest(id_pokemon);
+
+}
+
+void enviarSolicitudUbicacionPokenest(char id_pokemon){
+	//TODO enviar a mapa
+
+	//TODO asignar ubicaciones a ubicacionProximaPokenest
+	pokenestLocalizada = true;
+}
+
+void avanzarHaciaElPokenest(){
+
+
+
+	int movimientosRestantesEnX = (ubicacionProximaPokenest->x - ubicacionActual->x);
+	int movimientosRestantesEnY = (ubicacionProximaPokenest->y - ubicacionActual->y);
+
+	if(abs(movimientosRestantesEnX) >= abs(movimientosRestantesEnY)){
+		if(movimientosRestantesEnX > 0) ubicacionActual->x += 1;
+		if(movimientosRestantesEnX < 0) ubicacionActual->x -= 1;
+	}
+	else{
+		if(movimientosRestantesEnY > 0) ubicacionActual->y += 1;
+		if(movimientosRestantesEnY < 0) ubicacionActual->y -= 1;
+	}
+
+	//TODO informar movimiento al mapa
+}
+
+void atraparPokemon(){
+		t_pokemon * pokemonAtrapado;
+
+	//TODO enviar a mapa que quiero atrapar
+
+	//TODO podria recibir que hay un deadlock => batalla y return
+
+
+	//Todo copiar archivo de pokemon y asignarlo a pokemonAtrapado
+
+	list_add(pokemonesCapturados, pokemonAtrapado);
+
+	verificarNivelPokemon(pokemonAtrapado);
+	verificarSiQuedanObjetivosEnMapa();
+
+	pokenestLocalizada = false;
+
+}
+
+void verificarSiQuedanObjetivosEnMapa(){
+	if(queue_size(mapaActual->objetivos) == 0){
+			//TODO avisar a mapa que se cumplio objetivo
+			//TODO copiar medalla
+			desconectarseDeMapa();
+			queue_pop(metadata->viaje);
+
+			if(queue_size(metadata->viaje) == 0){
+				finDelJuego = true;
+			}
+			else{
+				conectarseConSiguienteMapa();
+			}
+			//TODO informar que ya termine el mapa
+		}
+	else{
+		//TODO informar a mapa que tengo mas objetivos
+	}
+}
+
+void verificarNivelPokemon(t_pokemon * pokemon){
+	if(pokemon->nivel > pokemonMasFuerte->nivel){
+
+		pokemonMasFuerte = pokemon;
+
+	}
+}
+
+void esperarTurno(){
+
+	//TODO esperar a que mapa ceda el turno
+
+}
+
+void realizarAccion(){
+
+	if(!pokenestLocalizada){
+		solicitarUbicacionDelProximoPokenest();
+		return;
+	}
+
+	if(estoyEnPokenest()){
+		atraparPokemon();
+		return;
+	}
+
+	if(!estoyEnPokenest()){
+		avanzarHaciaElPokenest();
+		return;
+	}
+
+}
+
+void enviarFinalizacionDeTurno(){
+	//TODO enviar a nucleo que finalizo el turno
+}
+
+bool estoyEnPokenest(){
+
+	return (ubicacionProximaPokenest->x == ubicacionActual->x) && (ubicacionProximaPokenest->y == ubicacionActual->y);
+}
+
+void imprimirLogro(){
+	//TODO imprimir por pantalla el logro
+}
+
+void batallaPokemon(){
+	//TODO enviar a mapa pokemonMasFuerte
+	//TODO recibo si vivo o muero
+
+	muerteEntrenador();
+}
+
+void muerteEntrenador(){
+	cantidadDeMuertes += 1;
+	puts("El entrenador a muerto debido a que perdio una batalla pokemon");
+
+	if(metadata->vidas > 0){
+		metadata->vidas -= 1;
+		desconectarseDeMapa();
+	}
+	else{
+		char  respuesta;
+		bool contestaConOtroCaracter = true;
+
+		printf("No quedan vidas disponibles, cantidad de reintentos: %d \n", metadata->reintentos);
+		puts("¿Desea reiniciar el juego?");
+		puts("1/0");
+
+		while(contestaConOtroCaracter){
+		scanf(respuesta);
+
+		switch(respuesta){
+
+		case 1:
+			metadata->reintentos += 1;
+			//todo borrar medallas
+			desconectarseDeMapa();
+			finalizarEntrenador();
+			inicializarSinmuertesNiReintentos();
+			contestaConOtroCaracter = false;
+			break;
+
+		case 0:
+			contestaConOtroCaracter = false;
+			exit(EXIT_SUCCESS);
+			break;
+
+		default:
+			puts("conteste con 1/0");
+		}
+		}
+	}
+
+}
+
+void desconectarseDeMapa(){
+
+	//TODO desconectarse de  mapa
+	//TODO borrar pokemones de directorio
+	list_clean(pokemonesCapturados);
+	pokenestLocalizada = false;
+
+}
+
+void finalizarEntrenador(){
+	free(metadata->nombre);
+	free(ubicacionActual);
+	queue_destroy_and_destroy_elements(metadata->viaje, (void*) destruirHojaDeViaje);
+	free(ubicacionProximaPokenest);
+	list_destroy(pokemonesCapturados);
+	queue_destroy(mapaActual->objetivos);
+	free(mapaActual);
+	free(pokemonMasFuerte);
+
+}
+
+void destruirHojaDeViaje(t_mapa * mapa){
+	free(mapa->nombre_mapa);
+	queue_destroy(mapa->objetivos);
+}
+
--- entrenador/src/main_entrenador.c
+++ entrenador/src/main_entrenador.c
@@ -9,37 +9,28 @@
 
 int main(int argc, char ** argv) {
 
-	socket_entrenador = -1;
+	signal(SIGUSR1,rutina);
+	signal(SIGTERM, rutina);
 
 	if (!chequear_argumentos(argc, TOTAL_ARGS)) {
 		return EXIT_FAILURE;
 	}
 
-	leer_metadata_mapa(argv[2]);
-	imprimir_metadata();
+	metadata_path = argv[2];
 
-	/*
-	 *  lo que tenga *** vuela despues
-	 */
+		inicializarEntrenador();
+		conectarseConSiguienteMapa();
 
-	int puerto_ejemplo = 9000; //***
-	char * ip_ejemplo = string_duplicate("127.0.0.1"); //***
+		while(!finDelJuego){
 
-	socket_entrenador = conectarse_a_un_mapa(puerto_ejemplo, ip_ejemplo);
+		esperarTurno();
+		realizarAccion();
+		enviarFinalizacionDeTurno();
 
-	if (socket_entrenador < 0) {
-		return EXIT_FAILURE;
-	} else {
-		puts("Me conecte al mapa"); //***
-	}
+		}
 
-	srand(time(NULL));
-	int random_number = rand();
-
-	enviar_datos_a_mapa(socket_entrenador, random_number, argv[1]);
-
-	close(socket_entrenador);
-	free(ip_ejemplo);
-
+		imprimirLogro();
+	
+	
 	return EXIT_SUCCESS;
 }
