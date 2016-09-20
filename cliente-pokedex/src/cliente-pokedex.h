e5b25a7afd9f2532d3b16f630e3685e69c00f90a
Julian Corvalan <jcorvalan93@hotmail.com>
2016-09-20 14:18:50 -0300

    Comienzo con el cliente pokedex

--- cliente-pokedex/src/cliente-pokedex.h
+++ cliente-pokedex/src/cliente-pokedex.h
@@ -8,6 +8,8 @@
 #ifndef CLIENTE_POKEDEX_H_
 #define CLIENTE_POKEDEX_H_
 
+#define FUSE_USE_VERSION 27
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,10 +17,16 @@
 #include <commons/string.h>
 #include <shared_configs.h>
 #include <shared_sockets.h>
+#include <fuse.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
 
 #define TOTAL_ARGS 2
 #define PORT 9000
 #define IP "127.0.0.1"
+
+#define DEFAULT_FILE_PATH "/"
 
 char * ip_pokedex;
 int puerto_pokedex;
@@ -26,5 +34,6 @@ char * directorio_montaje;
 int socket_pokedex;
 
 int conectar_con_servidor_pkdx();
+
 
 #endif /* CLIENTE_POKEDEX_H_ */
--- cliente-pokedex/src/main_cliente.c
+++ cliente-pokedex/src/main_cliente.c
@@ -7,6 +7,68 @@
 
 #include "cliente-pokedex.h"
 
+static int pkdx_getattr( const char * path, struct stat * st ) {
+
+	int res;
+
+	st->st_uid = getuid();
+	st->st_gid = getgid();
+
+	if (strcmp(path, "/") == 0) {
+		st->st_mode = S_IFDIR | 0755;
+		st->st_nlink = 2;
+	} else if (strcmp(path, DEFAULT_FILE_PATH) == 0) {
+		st->st_mode = S_IFREG | 0444;
+		st->st_nlink = 1;
+		st->st_size = 1024;
+	} else {
+		res = -ENOENT;
+	}
+
+	return res;
+}
+
+static int pkdx_readdir( const char * path, void * buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info * fi ) {
+
+	(void) offset;
+	(void) fi;
+
+	if (strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	filler( buffer, ".", NULL, 0 );
+	filler( buffer, "..", NULL, 0 );
+	//faltan los directorios/archivos default para mostrar
+
+	return 0;
+}
+
+static int pkdx_read( const char * path, char * buffer, size_t size, off_t offset, struct fuse_file_info * fi) {
+	size_t len;
+	(void) fi;
+
+
+
+	return size;
+}
+
+static int pkdx_open(const char *path, struct fuse_file_info *fi) {
+	if (strcmp(path, DEFAULT_FILE_PATH) != 0)
+		return -ENOENT;
+
+	if ((fi->flags & 3) != O_RDONLY)
+		return -EACCES;
+
+	return 0;
+}
+
+static struct fuse_operations pkdx_fuse_operations = {
+		.getattr = pkdx_getattr,
+		.readdir = pkdx_readdir,
+		.read = pkdx_read,
+		.open = pkdx_open,
+};
+
 int main(int argc, char ** argv) {
 
 	if (chequear_argumentos(argc, TOTAL_ARGS) == -1) {
@@ -20,12 +82,6 @@ int main(int argc, char ** argv) {
 
 	socket_pokedex = conectar_con_servidor_pkdx();
 
-	while (1) {
-		char message[1000];
-		scanf("%s", message);
 
-		send(socket_pokedex, message, strlen(message), 0);
-	}
-
-	return EXIT_SUCCESS;
+	return fuse_main(argc, argv, &pkdx_fuse_operations, NULL);
 }
